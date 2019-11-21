2a25defb244ed91d2426274e7574613c89c8c782
maiersk <poilyoungman@foxmail.com>
2019-11-21 15:59:27 +0800

    part1

---
+++ index.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+app.set("views", path.join(__dirname, 'views'));
+app.set("view engine", 'ejs');
+
+//app.use('/', indexRouter);
+app.use("/users", usersRouter);
+
+app.use(function(req, res, next) {
+    console.log('1');
+    next(new Error('error'));
+})
+
+app.use(function(req, res, next) {
+    console.log('2');
+    res.status(200).end();
+})
+
+app.use(function(err, req, res, next) {
+    console.log(err.stack);
+    res.status(500).send('Something broke!');
+})
+
+app.listen(80);
