1bc93080d8e41672b566cc76a81d5d4a7877e90a
maiersk <poilyoungman@foxmail.com>
2019-11-28 16:16:02 +0800

    修改signout拼写错误

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     })
     app.use("/signup", require("./signup"))
     app.use("/signin", require("./signin"))
-    app.use("/sigout", require("./signout"))
+    app.use("/signout", require("./signout"))
     app.use("/posts", require("./posts"))
     app.use("/comments", require("./comments"))
 
